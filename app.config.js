2a30fa0c2a348e417d745de7adde382dd5ba7f26
Meshack Mumo <67875053+MeshackMumo03@users.noreply.github.com>
2026-08-28 13:27:23 +0300

    Test: verify version-bump pre-commit hook fires

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   "expo": {
     "name": "chromacode",
     "slug": "chromacode",
-    "version": "2.71",
+    "version": "2.72",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
     "scheme": "chromacode",
